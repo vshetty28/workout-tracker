72854c0dedb236d8ce569130e55bd5f4d4ce469b
vshetty28 <vshetty1028@gmail.com>
2025-05-05 20:36:07 -0400

    logging search params

--- src/app/exercise-report/page.tsx
+++ src/app/exercise-report/page.tsx
@@ -8,14 +8,14 @@ const ExerciseReport = async ({ searchParams }: { searchParams: Promise<{ [key: 
 	const formatDate = (date: Date) => {
 		return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, "0")}-${String(date.getDate() + 1).padStart(2, "0")}`;
 	};
-	const { start = "1950-01-01", end = formatDate(new Date()), min = "0", max = "1440", sort = "w.date", order = "DESC" } = await searchParams;
+	const { start = "1950-01-01", end = formatDate(new Date()), min = "0", max = "2000", sort = "w.date", order = "DESC" } = await searchParams;
 	const startDate = new Date(Array.isArray(start) ? start[0] : start);
 	const endDate = new Date(Array.isArray(end) ? end[0] : end);
 	const sortCol = Array.isArray(sort) ? sort[0] : sort;
 	const sortOrder = Array.isArray(order) ? order[0] : order;
 	startDate.setDate(startDate.getDate() + 1);
 	endDate.setDate(endDate.getDate() + 1);
-
+	console.log(min, max);
 	const exercises: any = await getAllExercises(startDate, endDate, Number(min), Number(max), sortCol, sortOrder);
 
 	const averages: any = await getAverages(startDate, endDate, Number(min), Number(max));
